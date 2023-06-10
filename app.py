67bbc382f3c25562333c84256328662d5c51cdb2
Konstantin Gonikman <gonikman@me.com>
2023-06-10 22:15:18 +0200

    first

---
+++ app.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool | None = None
+
+
+@app.get("/")
+def read_root():
+    return {"key": "value"}
+
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id, "item_price": item.price}
