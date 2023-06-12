cad82edb252ccee99af87dacc964b0177ba41ea0
Konstantin Gonikman <gonikman@me.com>
2023-06-12 12:11:11 +0200

    test

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ class Item(BaseModel):
 
 @app.get("/", tags=["Root"])
 async def read_root():
-    return {"message": "Welcome to the API"}
+    return {"message": "Welcome to the API v.2"}
 
 
 @app.get("/items/{item_id}")
